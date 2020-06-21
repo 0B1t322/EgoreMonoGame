499e64b431374fd2c43a63f6f17dd761beace7b8
0B1t322 <dan.demin@gmail.com>
2020-06-21 23:49:07 +0300

    update console log

--- Eroge/Engine/ConsoleLog/ConsoleLog.cs
+++ Eroge/Engine/ConsoleLog/ConsoleLog.cs
@@ -1,25 +1,40 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Eroge.Engine.ConsoleLog
 {
-    public static class ConsoleLog
+    public class ConsoleLog
     {
         ///<summary> Print Okay Message in console </summary>
-        public static Action<object> OkayMess = (object obj) => PrintMessage(obj);
+        public static Action<object> OkayAction = (object obj) => Message(obj);
         
         ///<summary> Print Error Message in console </summary>
-        public static Action<object> ErrorMess = (object obj) => PrintErrorMessage(obj);
+        public static Action<object> ErrorAction = (object obj) => ErrorMessage(obj);
 
-        private static void PrintMessage(object message)
+        private static void Message(object message)
         {
             Console.WriteLine(message);
         }
 
-        private static void PrintErrorMessage(object message)
+        private static void ErrorMessage(object message)
         {
             Console.WriteLine(message);
         }
 
+        public static void PrintErrorMessage(string message)
+        {
+            Task task = new Task( ErrorAction, "[ERROR] " + message);
+            task.Start();
+            
+        }
+
+        public static void PrintMessage(string message)
+        {
+            Task task = new Task( OkayAction, message);
+            task.Start();
+        }
+
+        
     }
 }
